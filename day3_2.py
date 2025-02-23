b1efaef694de490f68306c358fdae7aaa0119661
CiepLap <adamulicho@gmail.com>
2025-02-24 00:30:13 +0100

    day3_2

--- day3_2.py
+++ day3_2.py
@@ -1,24 +1,46 @@
 from pathlib import Path
 import re
 import operator
+from typing import Generator, Tuple, Optional
 
 filepath = "input_3"
 
 
-def get_digits(input_string: str) -> list[list[int]]:
-    return [
-        [int(string_a), int(string_b)]
-        for string_a, string_b in (re.findall(r"mul\((\d+),(\d+)\)", input_string))
-    ]
+def filter_input(
+    input_string: str,
+) -> Generator[
+    Tuple[Optional[str], Optional[str], Optional[int], Optional[int]], None, None
+]:
+    return (
+        (
+            disable_keyword or None,
+            enable_keyword or None,
+            int(number_1) if number_1 else None,
+            int(number_2) if number_2 else None,
+        )
+        for disable_keyword, enable_keyword, number_1, number_2 in re.findall(
+            r"(don\'t\(\))|(do\(\))|mul\((\d+),(\d+)\)", input_string
+        )
+    )
 
 
-def sum_up_digits(digits_list: list[list[int]]) -> int:
+def sum_up_digits(
+    filtered_text: Generator[
+        Tuple[Optional[str], Optional[str], Optional[int], Optional[int]], None, None
+    ],
+) -> int:
     result = 0
-    for digit_a, digit_b in digits_list:
-        result += operator.mul(digit_a, digit_b)
+    enable = True
+    for disable_keyword, enable_keyword, number_a, number_b in filtered_text:
+        if disable_keyword:
+            enable = False
+        if enable_keyword:
+            enable = True
+        if enable and number_a and number_b:
+            result += operator.mul(number_a, number_b)
     return result
 
 
-reports = Path(filepath).read_text()
-digits_list = get_digits(reports)
-print(sum_up_digits(digits_list))
+reports: str = Path(filepath).read_text()
+filtered_reports = filter_input(reports)
+print(sum_up_digits(filtered_reports))
